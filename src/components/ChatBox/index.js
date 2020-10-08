41f01df244e14b52a86806d2356e1407a446948f
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2020-10-08 11:27:14 +0200

    feat(bot): response from bots available

--- src/components/ChatBox/index.js
+++ src/components/ChatBox/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Input from "../Input";
 import MessageBox from "../MessageBox";
-
+import Bot from "../../mock"
 class ChatBox extends Component {
   constructor(props) {
     super(props);
@@ -14,9 +14,55 @@ class ChatBox extends Component {
       }]
     }
     this.getMessage = this.getMessage.bind(this)
+    this.botMessage = this.botMessage.bind(this)
+
+  }
+
+  findActions(bot, value) {
+    return bot.actions
+      .map((actions) => actions.keyWords
+        .map((keyWord) => (keyWord === value ? actions.response() : false))
+        .filter((action) => (!action ? false : action)))
+      .filter((result) => result.length !== 0);
+  }
+
+  botResponse(message) {
+    Bot.forEach(element => {
+      const res = this.findActions(element, message)
+      if(res[0]) {
+        this.botMessage(element, res[0])
+      }
+    })
+  }
+
+  botMessage(bot, message) {
+    const { id, avatar, name } = bot;
+    const { messageArray } = this.state;
+    const newMessage = {
+      id: id,
+      avatar,
+      name,
+      content: message[0],
+      from: 'bot',
+      created_at: Date.now()
+    }
+
+    setTimeout(() => {
+      this.concatMessages(newMessage)
+    }, 1000)
+
+  }
+
+  concatMessages(newMessage) {
+    const { messageArray } = this.state;
+    this.setState({
+      messageArray: messageArray.concat(newMessage)
+    })
+    
   }
 
   getMessage(message) {
+    this.botResponse(message)
     const { messageArray } = this.state;
     const newMessage = {
       id: Math.floor(Math.random() * Math.floor(1000)),
@@ -24,9 +70,10 @@ class ChatBox extends Component {
       from: 'user',
       created_at: Date.now()
     }
-    this.setState({
-      messageArray: [...messageArray, newMessage]
-    })
+    this.concatMessages(newMessage)
+    // this.setState({
+    //   messageArray: [...messageArray, newMessage]
+    // })
   }
   render() {
     const { messageArray } = this.state;
